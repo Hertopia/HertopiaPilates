c187c77d9df877567941f5e94efbe491607010b8
Hertopia <hertopia.pilates@gmail.com>
2024-09-12 00:14:45 -0400

    Delete header.js

--- header.js
+++
@@ -1,8 +0,0 @@
-const burger = document.getElementById("burger");
-const navLinks = document.getElementById("navLinks");
-
-// 点击汉堡菜单时，显示或隐藏导航链接
-burger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-  burger.classList.toggle("toggle");
-});
